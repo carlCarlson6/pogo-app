0793041f18f45faa7c64110e486f05a1118757f4
carl <carlos.acitores@colba.es>
2024-10-02 09:27:36 +0200

    fixing ui on new account

--- pogo-mobile-app/app/index.tsx
+++ pogo-mobile-app/app/index.tsx
@@ -1,28 +1,29 @@
-import { Image, Pressable, Text, View } from "react-native";
+import { Image, Pressable, Text, TextInput, View } from "react-native";
 import { useRouter } from "expo-router";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center bg-black" >
-      
-      <View className="pt-64">
-        <Image source={require("@/assets/images/pogo-logo.png")}/>
-      </View>
-      
-      <View className="pt-32 gap-2">
-        <View>
-          <NewAccountButton />
+    <View className="bg-black h-screen">
+      <View className="flex items-center" >
+
+        <View className="pt-64">
+          <Image source={require("@/assets/images/pogo-logo.png")}/>
         </View>
+      
+        <View className="pt-32 gap-2">
+          <View>
+            <NewAccountButton />
+          </View>
         
-        <View>
-          <LoginButton />    
+          <View>
+            <LoginButton />    
+          </View>
+        </View>
+
+        <View className="pt-20">
+          <Image source={require("@/assets/images/social-login-icons.png")}/>
         </View>
       </View>
-
-      <View className="pt-20">
-        <Image source={require("@/assets/images/social-login-icons.png")}/>
-      </View>
-
     </View>
   );
 }
--- pogo-mobile-app/app/newAccount.tsx
+++ pogo-mobile-app/app/newAccount.tsx
@@ -1,67 +1,32 @@
 import { useRouter } from "expo-router";
-import { View, Text, Image, TextInput, Pressable, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from "react-native";
-
+import { View, Text, Image, TextInput, Pressable, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, StyleSheet } from "react-native";
 
 export default function NewAccount() {
-  const { goBack } = useNewAccount();
-
   return (
-    <View className="flex-1 flex-col bg-black gap-5 pt-20">
-      
-      <View className="pl-5">
-        <Pressable
-          className="w-16 -inset-x-5"
-          onPress={goBack}
-        >
-          <View className="pl-5">
-            <Image source={require("@/assets/images/Chevron.png")} />
-          </View>
-        </Pressable>  
-      </View>  
-  
+    <View className="bg-black h-screen pt-14 border-2 border-red-500">
+      <GoBackButton />
+
       <View className="border-4 w-52 h-52 -inset-x-10 border-white rounded-full flex items-center justify-center">
         <Image source={require("@/assets/images/photo-camera.png")}/>
       </View>
-  
-      <View className="gap-4 pr-4">
-        <View className="bg-zinc-800 py-2">
-          <TextInput
-            className="text-lg pl-4 text-white" 
-            placeholder="Nombre"
-          />
-        </View>
 
-        <View className="bg-zinc-800 py-2">
-          <TextInput 
-            className="text-lg pl-4"
-            placeholder="Apellido"
-          />
-        </View>
+      <TextInput 
+        className="bg-zinc-900 text-white"
+        style={[{fontSize: 20}]}
+      />
 
-        <View className="bg-zinc-800 py-2">
-          <TextInput 
-            className="text-lg pl-4"
-            placeholder="E-mail"
-          />
-        </View>
-
-        <View className="bg-zinc-800 py-2">
-          <TextInput 
-            className="text-lg pl-4"
-            placeholder="Password"
-            secureTextEntry={true}
-          />
-        </View>
-      </View>
-  
     </View>
   );
 }
 
-const useNewAccount = () => {
+const GoBackButton = () => {
   const router = useRouter();
-
-  return {
-    goBack: router.back
-  };
+  return (
+    <Pressable
+      className="pl-8 pb-6"
+      onPress={router.back}
+    >
+      <Image source={require("@/assets/images/Chevron.png")} /> 
+    </Pressable>
+  );
 }
